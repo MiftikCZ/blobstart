4ae496edfc1a8513abbea68e3e4fce094e1640bb
MiftikCZ <memertv17@gmail.com>
2023-04-22 12:19:22 +0200

    Add customization via url params

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,32 @@
-import { useState } from 'preact/hooks'
-import './app.css'
-import Clocks from './components/Clocks'
-import Links from './components/Links'
-import config from "../public/config.json"
+import { useState } from "preact/hooks";
+import "./app.css";
+import Clocks from "./components/Clocks";
+import Links from "./components/Links";
+import config from "../public/config.json";
 
 export function App() {
+  let searchParams= new URLSearchParams(window.location.search);
+  let selectedImage = config.images[Math.floor(Math.random() * config.images.length)]
   
   return (
     <>
-      <div class="card center">
-        <Clocks/>
-        <Links/>
-      </div>
-    <style>
-      {`:root {
-        --img-source: url('${config.images[Math.floor(Math.random() * config.images.length)]}');
-        --columns: ${config.grid.columns};
-        --max-width: ${config.grid.width};
-      }
-      ${config.colors.usegreentitle ? `.links .title {
-        color: mediumseagreen;
-      }` : ""}
+      <Clocks />
+      <Links />
+      <style>
+        {`:root { 
+          ${searchParams.has("bg") ?
+            `--img-source: #${searchParams.get("bg") || "141419"};` :
+            `--img-source: url('${selectedImage}');` 
+          }
+          ${searchParams.has("width") ?
+          `--max-width: ${searchParams.get("width") || "800"}px;` : ""}
+        }
+
+        ${searchParams.has("title") ? `.links .title {
+          color: #${searchParams.get("title") || "3cb371"};
+        }` : ""}      
       `}
-    </style>
+      </style>
     </>
-  )
+  );
 }
